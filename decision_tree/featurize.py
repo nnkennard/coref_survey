8074f03d4f9f4351622316be83857330d5e2f55d
Neha Nayak <nnayak@cs.umass.edu>
2019-12-02 18:48:31 -0500

    with lots of nonsense from scikit learn that should be obsolete

--- decision_tree/featurize.py
+++ decision_tree/featurize.py
@@ -1,4 +1,7 @@
 from sklearn import tree
+from sklearn.feature_selection import RFE
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LogisticRegressionCV
 import json
 import graphviz 
 import numpy as np
@@ -10,6 +13,27 @@ FALSE_POSITIVES = "fp"
 FALSE_NEGATIVES = "fn"
 
 CATEGORIES = [TRUE_POSITIVES, FALSE_POSITIVES, TRUE_NEGATIVES, FALSE_NEGATIVES]
+
+
+def get_top_n_RFE_features(model, n, examples, labels):
+  feature_indices = []
+
+  for i in range(n):
+    rfe = RFE(model, n+1)
+    fit = rfe.fit(examples, labels)
+    for i, included in enumerate(fit.support_):
+      if included and i not in feature_indices:
+        feature_indices.append(i)
+  
+  return feature_indices
+    
+
+def regroup_labels(labels):
+  replication_positives = [CATEGORIES.index(TRUE_POSITIVES), CATEGORIES.index(FALSE_POSITIVES)]
+  detection_positives = [CATEGORIES.index(TRUE_POSITIVES), CATEGORIES.index(TRUE_NEGATIVES)]
+  
+  return [int(i in replication_positives) for i in labels], [int(i in detection_positives) for i in labels]
+  
 
 def get_mention_list(dt_examples):
   mention_set = set()
@@ -28,7 +52,28 @@ def flatten_parse_spans(parse_spans, token_sentences):
     token_count += len(sentence)
 
   return overall_parse_span_map
-    
+  
+def _get_feature_importances(estimator, norm_order=1):
+    """Retrieve or aggregate feature importances from estimator"""
+    importances = getattr(estimator, "feature_importances_", None)
+
+    coef_ = getattr(estimator, "coef_", None)
+    if importances is None and coef_ is not None:
+        if estimator.coef_.ndim == 1:
+            importances = np.abs(coef_)
+
+        else:
+            importances = np.linalg.norm(coef_, axis=0,
+                                         ord=norm_order)
+
+    elif importances is None:
+        raise ValueError(
+            "The underlying estimator %s has no `coef_` or "
+            "`feature_importances_` attribute. Either pass a fitted estimator"
+            " to SelectFromModel or call fit before calling transform."
+            % estimator.__class__.__name__)
+
+    return importances  
 
 def get_onehot(value_list, target):
   onehot = [0] * len(value_list)
@@ -79,8 +124,8 @@ def main():
   dt_examples = []
   with open(example_file, 'r') as f:
     for line in f:
-      (doc_key, label, antecedent_start, antecedent_end, consequent_start,
-       consequent_end) = line.strip().split()
+      (doc_key, antecedent_start, antecedent_end, consequent_start,
+       consequent_end, label) = line.strip().split()
       dt_examples.append(DecisionTreeExample(doc_key, (int(antecedent_start), int(antecedent_end)),
                                         (int(consequent_start), int(consequent_end)), label))
   
@@ -95,22 +140,37 @@ def main():
     pair_features = mention_feature_map[example.antecedent] + mention_feature_map[example.consequent]
     examples.append(pair_features)
     labels.append(CATEGORIES.index(example.label))
+    if len(examples) == 1000:
+      break
 
   examples, labels = np.array(examples), np.array(labels)
+  print(examples.shape)
+  print(labels.shape)
+
+  replication_labels, error_detection_labels = regroup_labels(labels)
+
+  for x, y in zip(replication_labels, error_detection_labels):
+    print(x, y)
 
   feature_names = ["antecedent_len"] + ["antecedent_" + span for span in all_parse_labels]
   feature_names += ["consequent_len"] + ["consequent_" + span for span in all_parse_labels]
   
-  clf = tree.DecisionTreeClassifier(max_depth=5)
-  clf = clf.fit(examples, labels)
-  tree.plot_tree(clf) 
-  dot_data = tree.export_graphviz(
-                clf, filled=True, rounded=True, feature_names=feature_names,
-                class_names=CATEGORIES, out_file=None) 
-  graph = graphviz.Source(dot_data) 
-  graph.render("coref_dt")
 
+  # Replication
+  clf = LogisticRegressionCV(cv=5, random_state=0, max_iter=1000,
+          multi_class='multinomial').fit(examples, replication_labels)
+  sfm = SelectFromModel(clf, 10)
+  print(dir(sfm))
+  print(dir(clf))
+  print(_get_feature_importances(clf))
+  print(clf.coef_)
 
-
+  
+  print(list(feature_names[i] for i in get_top_n_RFE_features(clf, 15, examples, replication_labels)))
+  
+  clf = LogisticRegressionCV(cv=5, random_state=0, max_iter=1000,
+          multi_class='multinomial').fit(examples, error_detection_labels)
+  print(list(feature_names[i] for i in get_top_n_RFE_features(clf, 15, examples, error_detection_labels)))
+  
 if __name__ == "__main__":
   main()
